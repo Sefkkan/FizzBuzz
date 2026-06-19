2309e0102d888c2ecba703c7e30f73580397aa14
Sefkan <295601065+Sefkkan@users.noreply.github.com>
2026-06-20 00:38:16 +0200

    Add swagger UI

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,9 +1,10 @@
 using FizzBuzz;
 using FizzBuzz.Presentation;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options => options.OpenApiVersion = OpenApiSpecVersion.OpenApi3_0);
 builder.Services.AddProblemDetails();
 builder.Services.AddFizzBuzz();
 
@@ -12,6 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "FizzBuzz API"));
 }
 
 app.UseHttpsRedirection();
